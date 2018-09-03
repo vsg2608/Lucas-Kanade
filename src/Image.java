b5facff53c11454f015b7d1412649ff6ee188d41
Vishesh Goyal <vsg2608@gmail.com>
2018-09-04 03:07:58 +0530

    Image displayed

--- src/Image.java
+++ src/Image.java
@@ -1,58 +1,122 @@
+import java.awt.AlphaComposite;
+import java.awt.BorderLayout;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class Image {
-	public float[][] color;
-	private int width;
-	private int height;
-	public int totalColors=255;
 	
-	public Image(int width, int height, float[][]color) {
-		this.color=color;
-		this.height=height;
-		this.width=width;
+	
+	public static BufferedImage readImage(BufferedImage image, String Input_Path) {
+		try {
+			File input_file= new File(Input_Path);
+			image = ImageIO.read(input_file);
+			System.out.println("Reading complete.");
+			return image;
+		}
+		catch(IOException e) {
+			System.out.println("Error:"+ e);
+		}
+		return image;
 	}
 	
-	public int getHeight() {
-		return height;
+	public static void writeImage(BufferedImage image, String Output_FilePath) {
+		//WRITE IMAGE
+		try {
+			File Output_File= new File(Output_FilePath);
+			ImageIO.write(image, "jpg", Output_File);
+			
+			System.out.println("Writing Complete");
+			
+		}
+		catch(IOException e) {
+			System.out.println("Error wiriting image: "+e);
+		}
 	}
-	
-	public int getWidth() {
-		return width;
-	}
-	
-	public  float[][] spaceDerivative(){
-		float[][] derivative= new float[this.getWidth()*this.getHeight()][2];
-		int n=0;
-		for(int x=0;x<this.getWidth();x++) {
-			for(int y=0;y<this.getHeight();y++) {
-				if(x==0)
-					derivative[n][0]=(this.color[x+1][y]-this.color[x][y])/totalColors;
-				else if(x==this.getWidth()-1)
-					derivative[n][0]=(this.color[x][y]-this.color[x-1][y])/totalColors;
-				else 
-					derivative[n][0]=(this.color[x+1][y]-this.color[x-1][y])/2/totalColors;
-				if(y==0)
-					derivative[n][1]=(this.color[x][y+1]-this.color[x][y])/totalColors;
-				else if(y==this.getHeight()-1)
-					derivative[n][1]=(this.color[x][y]-this.color[x][y-1])/totalColors;
-				else 
-					derivative[n][1]=(this.color[x][y+1]-this.color[x][y-1])/2/totalColors;
-				n++;
+
+	public static BufferedImage convertToBW(BufferedImage image) {
+		int width = image.getWidth();
+		int height= image.getHeight();
+		//Converts colored image into black and white Image
+		for(int x=0;x< width;x++) {
+			for(int y=0;y<height;y++) {
+				int p=image.getRGB(x, y);
+				int a = (p>>24) & 0xff;
+			    int r = (p>>16) & 0xff;
+			    int g = (p>>8) & 0xff;
+			    int b = p & 0xff;
+			    int BW=(Max3(r,g,b)+Min3(r,g,b))/2;
+			    p = (a<<24) | (BW<<16) | (BW<<8) | BW;
+			    image.setRGB(x, y, p);
 			}
 		}
-		return derivative;
+		return image;
 	}
 	
-	public float[] timeDerivative(Image image){
-		float[] derivative= new float[this.getWidth()*this.getHeight()];
-		int n=0;
-		for(int x=0;x<this.getWidth();x++) {
-			for(int y=0;y<this.getHeight();y++) {
-				derivative[n]=-(image.color[x][y]-this.color[x][y])/totalColors;
-				n++;
-			}
-		}
-		
-		return derivative;
+	
+	public static BufferedImage resizeImage(final java.awt.Image image, int width, int height) {
+	    int targetw = 0;
+	    int targeth = 75;
+
+	    if (width > height)targetw = 112;
+	    else targetw = 50;
+
+	    do {
+	        if (width > targetw) {
+	            width /= 2;
+	        }
+
+	        if (height > targeth) {
+	            height /= 2;
+	        }
+	    } while (width > targetw || height > targeth);
+
+	    final BufferedImage bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+	    final Graphics2D graphics2D = bufferedImage.createGraphics();
+	    graphics2D.setComposite(AlphaComposite.Src);
+	    graphics2D.setRenderingHint(RenderingHints.KEY_INTERPOLATION,RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+	    graphics2D.setRenderingHint(RenderingHints.KEY_RENDERING,RenderingHints.VALUE_RENDER_QUALITY);
+	    graphics2D.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
+	    graphics2D.drawImage(image, 0, 0, width, height, null);
+	    graphics2D.dispose();
+
+	    return bufferedImage;
 	}
 	
+	public static void displayImage(BufferedImage image) {
+		//Displaying image
+			JFrame frame = new JFrame("Image");
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			JLabel lbl= new JLabel();
+			lbl.setIcon(new ImageIcon(image));
+			frame.getContentPane().add(lbl, BorderLayout.CENTER);
+			frame.pack();
+			frame.setLocationRelativeTo(null);
+			frame.setVisible(true);
+	}
+	
+	public static int Max3(int a, int b, int c) {
+		if(a>=b && a>=c) 
+			return a;
+		else if(b>=c && b>=a)
+			return b;
+		else 
+			return c;
+	}
+	
+	public static int Min3(int a, int b, int c) {
+		if(a<=b && a<=c) 
+			return a;
+		else if(b<=c && b<=a)
+			return b;
+		else 
+			return c;
+	}
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,3 @@
-import java.awt.AlphaComposite;
-import java.awt.Graphics2D;
-import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -16,28 +13,26 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedImage image1=null;
 		BufferedImage image2=null;
-		image1=readImage(image1,"res/Screenshot (8).png");
-		image2=readImage(image2,"res/Screenshot (9).png");
+		image1=Image.readImage(image1,"res/Screenshot (8).png");
+		image2=Image.readImage(image2,"res/Screenshot (9).png");
 		
 		int width=image1.getWidth();		//Image width
 		int height=image1.getHeight();		//Image height
 		
 		//Code to resize image if required
-//		image1= resizeImage(image1, width, height);
-//		image2= resizeImage(image2, width, height);
+//		image1= Image.resizeImage(image1, width, height);
+//		image2= Image.resizeImage(image2, width, height);
 		
 		width=image1.getWidth();					//Recalculation of width after resizing
 		height=image1.getHeight();					//Recalculation of height after resizing
 		
-		image1=convertToBW(image1);					//Convert to Black and White
+		image1=Image.convertToBW(image1);					//Convert to Black and White
 		float[][] color= getColorMatrix(image1);	//Get a single color matrix
-		Image img1=new Image(width,height,color);	//Initializing new frame image
-		image2=convertToBW(image2);					//Convert to Black and white image for second frame
+		imageMatrix img1=new imageMatrix(width,height,color);	//Initializing new frame image
+		image2=Image.convertToBW(image2);					//Convert to Black and white image for second frame
 		color= getColorMatrix(image2);				//Get a single color matrix
-		Image img2=new Image(width,height,color);	//Initialization of second frame
+		imageMatrix img2=new imageMatrix(width,height,color);	//Initialization of second frame
 		
-		writeImage(image1,"res/Output1.jpg");		//Writing black and white image
-		writeImage(image2,"res/Output2.jpg");		//Writing black and white image
 		
 		int noOfPixels=width*height;				//Total no of pixels
 		float[][] Ixy= img1.spaceDerivative();		//space derivative matrix
@@ -72,33 +67,24 @@ public class Main {
 		
 		System.out.println(width+" "+height);
 		System.out.println(v[0]+" "+v[1]);
+		
+		
+	    int white = (254<<24) | (255<<16) | (255<<8) | 255;
+	    int red = (254<<24) | (255<<16) | (0<<8) | 0;
+	    
+	    for(int i=0;i<25;i++)
+	    	image1.setRGB(X+i, (int) (Y+i*v[1]/v[0]), white);
+	    image1.setRGB(X, (int) (Y), red);
+		
+		
+		Image.displayImage(image1);				//Display image on window
+		Image.writeImage(image1,"res/Output0.jpg");		//Writing black and white image
+		Image.writeImage(image2,"res/Output1.jpg");		//Writing black and white image
 			
 	}
 	
-	
-	
-	
-	////////////////////////////////////////////////////////////////////////////
-	
-	public static BufferedImage convertToBW(BufferedImage image) {
-		int width = image.getWidth();
-		int height= image.getHeight();
-		//Converts colored image into black and white Image
-		for(int x=0;x< width;x++) {
-			for(int y=0;y<height;y++) {
-				int p=image.getRGB(x, y);
-				int a = (p>>24) & 0xff;
-			    int r = (p>>16) & 0xff;
-			    int g = (p>>8) & 0xff;
-			    int b = p & 0xff;
-			    int BW=(Max3(r,g,b)+Min3(r,g,b))/2;
-			    p = (a<<24) | (BW<<16) | (BW<<8) | BW;
-			    image.setRGB(x, y, p);
-			}
-		}
-		return image;
-	}
-	
+////////////////////////////////////////////////////////////////////////////
+		
 	public static float[][] getColorMatrix(BufferedImage image){
 		int width = image.getWidth();
 		int height= image.getHeight();
@@ -112,80 +98,6 @@ public class Main {
 			}
 		}
 		return color;
-	}
-	 
-	public static BufferedImage readImage(BufferedImage image, String Input_Path) {
-		try {
-			File input_file= new File(Input_Path);
-			image = ImageIO.read(input_file);
-			System.out.println("Reading complete.");
-			return image;
-		}
-		catch(IOException e) {
-			System.out.println("Error:"+ e);
-		}
-		return image;
-	}
-	
-	public static void writeImage(BufferedImage image, String Output_FilePath) {
-		//WRITE IMAGE
-		try {
-			File Output_File= new File(Output_FilePath);
-			ImageIO.write(image, "jpg", Output_File);
-			
-			System.out.println("Writing Complete");
-			
-		}
-		catch(IOException e) {
-			System.out.println("Error wiriting image: "+e);
-		}
-	}
-	
-	public static int Max3(int a, int b, int c) {
-		if(a>=b && a>=c) 
-			return a;
-		else if(b>=c && b>=a)
-			return b;
-		else 
-			return c;
-	}
-	
-	public static int Min3(int a, int b, int c) {
-		if(a<=b && a<=c) 
-			return a;
-		else if(b<=c && b<=a)
-			return b;
-		else 
-			return c;
-	}
-	
-	public static BufferedImage resizeImage(final java.awt.Image image, int width, int height) {
-	    int targetw = 0;
-	    int targeth = 75;
-
-	    if (width > height)targetw = 112;
-	    else targetw = 50;
-
-	    do {
-	        if (width > targetw) {
-	            width /= 2;
-	        }
-
-	        if (height > targeth) {
-	            height /= 2;
-	        }
-	    } while (width > targetw || height > targeth);
-
-	    final BufferedImage bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-	    final Graphics2D graphics2D = bufferedImage.createGraphics();
-	    graphics2D.setComposite(AlphaComposite.Src);
-	    graphics2D.setRenderingHint(RenderingHints.KEY_INTERPOLATION,RenderingHints.VALUE_INTERPOLATION_BILINEAR);
-	    graphics2D.setRenderingHint(RenderingHints.KEY_RENDERING,RenderingHints.VALUE_RENDER_QUALITY);
-	    graphics2D.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
-	    graphics2D.drawImage(image, 0, 0, width, height, null);
-	    graphics2D.dispose();
-
-	    return bufferedImage;
 	}
 	
 	public static float gaussianFunction(double d) {
